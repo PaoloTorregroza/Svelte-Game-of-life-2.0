f118a3378e0accddbc178ea6bdb2bd5093f67f78
PaoloTorregroza <paolinskypdtn@gmail.com>
2024-10-15 00:28:08 -0500

    vite settings for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Svelte-Game-of-life-2.0/',
   plugins: [svelte()],
 })
